f666a6520040eeff2ed8705ba36c6665130f6560
trichaabdelouahed <118966828+trichaabdelouahed@users.noreply.github.com>
2025-05-24 19:02:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/') 
 def home(): 
-  return "TP4 : Bonjour depuis Render !  l'app Flask est bien en ligne." 
+  return "TP4 : Bonjour depuis Render !  l'app Flask est bien en ligne..." 
 if __name__ == '__main__': 
   app.run()
