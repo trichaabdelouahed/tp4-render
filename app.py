c6d399d56f5a2b94d479c652a6474ee61c864197
trichaabdelouahed <118966828+trichaabdelouahed@users.noreply.github.com>
2025-05-24 18:34:20 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def home(): 
+  return "TP4 : Bonjour depuis Render !  l'app Flask est bien en ligne." 
+if __name__ == '__main__': 
+  app.run()
